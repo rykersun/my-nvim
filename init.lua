5fe733fad1358956bb4bd3cc0a82a10e40b5ba04
Ryker Sun <rykersun2002@gmail.com>
2023-01-21 15:27:40 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,6 +5,6 @@ require("c") -- config
 
 -- Colorscheme settings
 vim.opt.cursorline = true
-require("colorscheme.vscode")
--- require("colorscheme.github")
+-- require("colorscheme.vscode")
+require("colorscheme.github")
 -- require("colorscheme.tokyonight")
