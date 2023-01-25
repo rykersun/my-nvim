ee3f7bbe37fd2927bd643f1c9bc274c37d34a5ec
Ryker Sun <rykersun2002@gmail.com>
2023-01-25 10:17:38 +0800

    Update p.lua

--- lua/p.lua
+++ lua/p.lua
@@ -1,5 +1,12 @@
 local ensure_packer = function()
   local fn = vim.fn
+  -- Autocommand that reloads neovim whenever you save the plugins.lua file
+  vim.cmd [[
+    augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost p.lua source <afile> | PackerSync
+    augroup end
+  ]]
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
