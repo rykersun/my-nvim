b7baa1164e0bc28b928983f7d173bf4ee129afa6
Ryker Sun <rykersun2002@gmail.com>
2023-04-25 02:04:25 +0800

    Comment nvim-tree config (still have problem)

--- lua/c.lua
+++ lua/c.lua
@@ -136,26 +136,26 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-        { key = "?", action = "toggle_help" },
-        { key = "<CR>", action = "cd" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
-vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle!<CR>')
+-- require("nvim-tree").setup({
+--   sort_by = "case_sensitive",
+--   view = {
+--     adaptive_size = true,
+--     mappings = {
+--       list = {
+--         { key = "u", action = "dir_up" },
+--         { key = "?", action = "toggle_help" },
+--         { key = "<CR>", action = "cd" },
+--       },
+--     },
+--   },
+--   renderer = {
+--     group_empty = true,
+--   },
+--   filters = {
+--     dotfiles = true,
+--   },
+-- })
+-- vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle!<CR>')
 require("bufferline").setup{}
 vim.keymap.set('n', '<leader>w', '<C-w>')
 vim.keymap.set('n', '<leader>n', ':noh<CR>')
