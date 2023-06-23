c5d1c1d50977fc213a728cd9f657a1db91269665
ryker <ryker@atlantic>
2023-06-23 12:26:23 +0800

    Fix lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -126,3 +126,6 @@ cmp.setup {
   },
 }
 lsp.setup()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = capabilities })
